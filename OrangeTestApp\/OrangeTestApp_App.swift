c5f51043e93438ed7bf81d6a3cdd6f07428d517a
turkankirill <t.kirill@icloud.com>
2022-07-15 07:49:18 +0300

    Initial Commit

---
+++ OrangeTestApp\/OrangeTestApp_App.swift
@@ -0,0 +1,17 @@
+//
+//  OrangeTestApp_App.swift
+//  OrangeTestApp\
+//
+//  Created by Kirill Turkan on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct OrangeTestApp_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
